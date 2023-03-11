57a60f5208ace74c4bb2f800bab4b6ba98765a8b
= <=>
2023-03-11 13:49:47 -0800

    Save

--- index.js
+++ index.js
@@ -1,62 +1,50 @@
 let NumTown = document.getElementById('NumTown');
 let board = document.getElementById('board');
-let submit = document.getElementById('submit'); 
+let submit = document.getElementById('submit');
 let Header = document.getElementById('Header');
-let createTowns = document.getElementById('createTowns');
+let createHomes = document.getElementById('createHomes');
 let houses = document.getElementById('houses');
-let submitHouse = document.getElementById('submitHouse');
-
-createTowns.style.display = "none";
-submitHouse.style.display = "none";
-
-submit.addEventListener("click" , () => {
-    if(!(NumTown.value == "" || NumTown.value == null)){
-        if(!(NumTown.value > 30 || NumTown.value < 0))
-        {
-            let numOfTown = NumTown.value; // Number Of town
-            
-            // css
-            Header.style.display = "none";
-            submit.style.display = "none";
-            createTowns.style.display = "block";
-            
-            // code
-            for(let x = 0; x<numOfTown; x++) // create the number of the town's
-            {   
+let result = document.getElementById('result');
+result.style.display = "none";
+let InputsOfUser = document.getElementById('InputsOfUser');
+createHomes.style.display = "none";
+submit.addEventListener("click", () => {
+    if (!(NumTown.value == "" || NumTown.value == null)) {
+        if (!(NumTown.value > 30 || NumTown.value < 0)) {
+            let numOfTown = NumTown.value;
+            // create Towns
+            for (let x = 0; x < numOfTown; x++) {
+                let div = document.createElement("div");
+                let label = document.createElement("label");
+                label.innerHTML = "How many House in (Town " + (x + 1) + ")";
                 let input = document.createElement("input");
                 input.classList.add("towns");
+                input.placeholder = "Enter Number : "
                 input.type = "number";
-                input.placeholder = "Enter Number of House (TownShip of " + (x+1) + ")";                
-                board.appendChild(input);
-            }   
-            
-            let towns = document.getElementsByClassName("towns");
-            createTowns.addEventListener("click" , () => {
-                board.style.display = "none";
-                createTowns.style.display = "none";
+                div.appendChild(label);
+                div.appendChild(input);
+                board.appendChild(div);
+            }
+            let towns = document.getElementsByClassName('towns');
+            Header.style.display = "none";
+            createHomes.style.display = "block";
+            createHomes.addEventListener("click", () => {
                 let obj = {};
-                for(let x = 0;x<towns.length; x++){ 
-                    for(let y = 0; y < towns[x].value ; y++)
-                    {
-                        let House = window.prompt("Enter Home [ " + (x + 1) + " ]");
-                        obj.town = House;
+                for (let x = 0; x < towns.length; x++) {
+                    let TownNum = x;
+                    for (let y = 0; y < towns[x].value; y++) {
+                        let House = prompt("Enter Dimention For House[ "+ (y+1) +" ] of the Town " + (x + 1) + "");
+                        obj[TownNum] += "|" + House;
                     }
                 }
-                console.log(obj);
-            });  
-            
-            
-            // submitHouse.addEventListener("click" , () => {
-            //     let arr = [];
-            //     let townHouse = document.getElementsByClassName('townHouse');
-            //     for(let x = 0; x < townHouse.length ; x++   )
-            //     {
-            //         arr.push(townHouse[x].value);
-            //     }
-            //     operation(arr , towns);
-            // });
+                for (let x in obj) {
+                    obj[x] = obj[x].replace(/[a-z]/g, "");
+                    obj[x] = obj[x].split("|");
+                }
+                operation(obj);
+            });
         }
-        else{
+        else {
             alert("Entered Value was not allowed less than 0 or greater than 30");
             NumTown.value = "";
         }
@@ -66,14 +54,41 @@ submit.addEventListener("click" , () => {
     }
 });
 
-// function operation(array , towns){
-//     let arr = [];
-//     for(let i = 0; i < towns.length; i++){
-//         arr.push("Town");
-//         for(let x = 0; x <towns[i].value ; x++)
-//         {
-//             arr.push(array[x]);
-//         }
-//     }
-//     console.log(arr);
-// }
+function operation(obj) {
+    for (let i = 0; i < Object.keys(obj).length; i++) {
+        let blankArr = [0, 0, 0, 0];
+        for (let j = 1; j < Object.keys(obj[i]).length; j++) {
+            let x = obj[i][j];
+            let oldF = x.split(',');
+            let intArr = [];
+            for (let ele = 0; ele < oldF.length; ele++) {
+                intArr.push(parseInt(oldF[ele]));
+            }
+            for (let ele = 0; ele < oldF.length; ele++) {
+                blankArr[ele] += intArr[ele];
+            }
+        }
+        display(blankArr);
+    }
+}
+function display(array) {
+    let arr = [];
+    let RSH = [3 , 4 , 6];
+    let total = 0;
+    for(let i = 0 ; i < array.length - 1; i++)
+    {
+        arr[i] = array[i+1] * RSH[i];
+        total += arr[i];
+    } 
+    let accColor = total * (1 / 3) * 1.5;
+    let normalColor = total * (2 / 3) * 2.25;
+    let TimeAcc = total * (1 / 3) * 2.5;
+    let Timenormal = total * (2 / 3) * 3.25;
+    let TotalTime = TimeAcc + Timenormal;
+    // total hours, accent qty, regular qty.   
+    InputsOfUser.style.display = "none";
+    result.style.display = "block";
+    let Showtxt = document.getElementById('Showtxt');
+    Showtxt.innerHTML +=  "Home Ammount :: Time : " + TotalTime + " accent colour : " + accColor + " regular colour : " + normalColor + "<br><br><br>";
+    console.log(TotalTime + " " + accColor + " " + normalColor);
+}
